bdaa4e1417d32ff971c088e7c2186cb30c32c13f
sagor-skp <sagorkpandit@gmail.com>
2025-02-14 17:00:45 +0600

    a

--- 15b_prims_mst.cpp
+++ 15b_prims_mst.cpp
@@ -1,3 +1,4 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 #define v 6
@@ -48,4 +49,62 @@ int main(){
 
     findmst(graph);
     return 0;
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+int selectminvertex(vector<int>&value,vector<bool>&setmst,int v){
+    int min=INT_MAX;
+    int vertex;
+    for(int i=0;i<v;i++){
+        if(setmst[i]==false && value[i]<min){
+            min=value[i];
+            vertex=i;
+        }
+    }
+    return vertex;
+}
+void findmst(vector<vector<int>>&graph,int v){
+    vector<int>parent(v);
+    vector<int>value(v,INT_MAX);
+    vector<bool>setmst(v,false);
+    parent[0]=-1;
+    value[0]=0;
+    for(int i=0;i<v-1;i++){
+        int u=selectminvertex(value,setmst,v);
+        setmst[u]=true;
+        for(int j=0;j<v;j++){
+            if(graph[u][j]!=0 && setmst[j]==false && graph[u][j]<value[j]){
+                value[j]=graph[u][j];
+                parent[j]=u;
+            }
+        }
+    }
+
+
+    // Print MST and Total Weight
+
+    int totalweight = 0;
+    cout << "\nMinimum Spanning Tree:\n";
+    for (int i = 1; i < v; i++)
+    {
+        cout << "u->v: " << parent[i] << "->" << i << " wt = " << graph[parent[i]][i] << endl;
+        totalweight += graph[parent[i]][i];
+    }
+    cout << "Total weight = " << totalweight << endl;
+
+}
+int main(){
+    int v;
+    cout<<"Enter vertex :";
+    cin>>v;
+    vector<vector<int>>graph(v,vector<int>(v));
+    cout << "Enter the adjacency matrix for the graph (" << v << "x" << v << "):\n";
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            cin>>graph[i][j];
+        }
+    }
+    findmst(graph,v);
+    return 0;
+
 }
--- practise1.cpp
+++ practise1.cpp
@@ -188,7 +188,7 @@ int main(){
     }
     bfs(1);
 }*/
-//primsalgo
+/*//primsalgo
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -237,5 +237,109 @@ int main(){
     {0,4,8,2,0,7},
     {0,0,0,3,7,0}};
     findmst(graph);
+    return 0;
+}*/
+
+/*//prims
+#include<bits/stdc++.h>
+using namespace std;
+#define v 6
+int selectminvertex(vector<int>&value,vector<bool>&setmst){
+    int min=INT_MAX;
+    int vertex;
+    for(int i=0;i<v;i++){
+        if(setmst[i]==false && value[i]<min){
+            min=value[i];
+            vertex=i;
+        }
+    }
+    return vertex;
+}
+void primsalgo(int graph[v][v]){
+    vector<int>value(v,INT_MAX);
+    vector<bool>setmst(v,false);
+    int parent[v];
+    parent[0]=-1;
+    value[0]=0;
+    
+    for(int i=0;i<v-1;i++){
+        int u=selectminvertex(value,setmst);
+        setmst[u]=true;
+        for(int j=0;j<v;j++){
+            if(graph[u][j]!=0 && setmst[j]==false && graph[u][j]<value[j]){
+                value[j]=graph[u][j];
+                parent[j]=u;
+            }
+        }
+       
+    }
+    int totalw=0;
+    for(int i=1;i<v;i++){
+        cout<<"U->V : "<<parent[i]<<"->"<<i<<" wt :"<<graph[parent[i]][i]<<endl;
+        totalw+=graph[parent[i]][i];
+    }
+    cout<<"total weight="<<totalw;
+}
+int main(){
+    int graph[v][v]={{0,4,6,0,0,0},
+    {4,0,6,3,4,0},
+    {6,6,0,1,0,0},
+    {0,3,1,0,2,3},
+    {0,4,0,2,0,7},
+    {0,0,0,3,7,0}};
+    primsalgo(graph);
+    return 0;
+}*/
+
+//kruskal
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+int parent[N];
+int size[N];
+void make(int m){
+    parent[m]=m;
+    size[m]=1;
+}
+int find(int c){
+    if(parent[c]==c) return c;
+    return parent[c]=find(parent[c]);
+}
+int Union(int a,int b){
+    int aa=find(a);
+    int bb=find(b);
+    if(aa!=bb){
+        if(size[aa]<size[bb]){
+            swap(aa,bb);
+        }
+        parent[bb]=aa;//aa big
+        size[aa]+=size[bb];
+    }
+
+}
+int main(){
+    int n,e;
+    cin>>n>>e;
+    vector<pair<int,pair<int,int>>>edge;
+    for(int i=0;i<e;i++){
+        int wt,u,v;
+        cin>>u>>v>>wt;
+        edge.push_back({wt,{u,v}});
+    }
+    sort(edge.begin(),edge.end());
+    for(int i=1;i<=n;i++){
+        make(i);
+    }
+    int totalweight=0;
+    for(auto &it:edge){
+        int wt=it.first;
+        int u=it.second.first;
+        int v=it.second.second;
+        if(find(u)==find(v))continue;
+        Union(u,v);
+        totalweight+=wt;
+        cout<<u<<"   "<<v<<endl;
+    }
+    cout<<totalweight;
     return 0;
 }
